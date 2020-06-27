287a39c66d92e247144d0777334422b36fb07926
JonatasFernandesPimenta <jonatasfernandespimenta@gmail.com>
2020-06-27 10:42:28 -0300

    Added existing files

---
+++ StartingWTensorFlow/main.py
@@ -0,0 +1,69 @@
+import numpy as np
+import tensorflow as tf
+from datetime import datetime
+from sklearn.datasets import fetch_california_housing
+from sklearn.preprocessing import StandardScaler
+import os
+
+os.system('cls')
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+
+now = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+root_logdir = 'tf_logs'
+logdir = '{}/run-{}/'.format(root_logdir, now)
+
+housing = fetch_california_housing()
+m, n = housing.data.shape
+housing_data_plus_bias = np.c_[np.ones((m, 1)), housing.data]
+
+scaler = StandardScaler()
+scaled_housing_data = scaler.fit_transform(housing.data)
+scaled_housing_data_plus_bias = np.c_[np.ones((m, 1)), scaled_housing_data]
+
+n_epochs = 1000
+learning_rate = 0.01
+
+X = tf.placeholder(tf.float32, shape=(None, n + 1), name="X")
+y = tf.placeholder(tf.float32, shape=(None, 1), name="y")
+
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+training_op = optimizer.minimize(mse)
+
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+
+batch_size = 100
+n_batches = int(np.ceil(m / batch_size))
+
+def fetch_batch(epoch, batch_index, batch_size):
+    np.random.seed(epoch * n_batches + batch_index)
+    indices = np.random.randint(m, size=batch_size)
+    X_batch = scaled_housing_data_plus_bias[indices]
+    y_batch = housing.target.reshape(-1, 1)[indices]
+    return X_batch, y_batch
+
+mse_summary = tf.summary.scalar('MSE', mse)
+file_writer = tf.summary.FileWriter(logdir, tf.get_default_graph())
+
+with tf.Session() as sess:
+    sess.run(init)
+    for epoch in range(n_epochs):
+        for batch_index in range(n_batches):
+            X_batch, y_batch = fetch_batch(epoch, batch_index, batch_size)
+            if batch_index % 10 == 0:
+                summary_str = mse_summary.eval(feed_dict={X: X_batch, y: y_batch})
+                step = epoch * n_batches + batch_index
+                file_writer.add_summary(summary_str, step)
+            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+
+    best_theta = theta.eval()
+    print(best_theta)
+    save_path = saver.save(sess, './my_model_final.ckpt')
+
+file_writer.close()
